2995740a0674abb8c785f77a755dc41c2359c1f0
evan twidwell <evanski@gmail.com>
2025-06-10 19:43:15 -0700

    added favicon

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def post_list(request):
     posts = Post.objects.order_by('-date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
--- portfolio_blog/urls.py
+++ portfolio_blog/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog_views.post_list, name='home'),
     path('<slug:slug>/', blog_views.post_detail, name='post_detail'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')), name='favicon'),
 ]
