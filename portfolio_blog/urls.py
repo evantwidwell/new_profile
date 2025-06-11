8fdd23fca59603d8e56fbcfbcfc4af4515cf95e5
evan twidwell <evanski@gmail.com>
2025-06-11 08:53:10 -0700

    move urls

--- portfolio_blog/urls.py
+++ portfolio_blog/urls.py
@@ -6,8 +6,8 @@ from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog_views.post_list, name='home'),
-    path('<slug:slug>/', blog_views.post_detail, name='post_detail'),
     path('testing/', blog_views.testing, name='testing'),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')), name='favicon'),
+    path('', blog_views.post_list, name='home'),
+    path('<slug:slug>/', blog_views.post_detail, name='post_detail'),
 ]
