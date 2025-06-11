00118489c0e7aa4b2b89a5fe12f1064d4ea18f79
evan twidwell <evanski@gmail.com>
2025-06-10 20:35:27 -0700

    try again

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,19 @@ from .models import Post
 def post_list(request):
     try:
         posts = Post.objects.order_by('-date')
+        if not posts.exists():
+            return render(request, 'blog/post_list.html', {
+                'posts': [],
+                'message': 'No posts yet. Check back soon!'
+            })
         return render(request, 'blog/post_list.html', {'posts': posts})
     except Exception as e:
         if settings.DEBUG:
             raise e
-        return render(request, 'blog/post_list.html', {'posts': [], 'error': 'Unable to load posts'})
+        return render(request, 'blog/post_list.html', {
+            'posts': [],
+            'error': 'Unable to load posts'
+        })
 
 
 def post_detail(request, slug):
